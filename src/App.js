0acce7935842be2e0dbd4cbe50f4439937fc8357
Rahul Aasendiya <rahul.a@jointworksolutions.com>
2022-03-10 17:23:29 +0530

    final Burer App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
-        <h1>Hello Burger App</h1>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
